25e63fd93969a5f36f902a4766d2702e99144ea2
David O'Kelly <davidokellyloesberg@gmail.com>
2023-12-19 12:32:40 +0000

    before changes to augmentation

--- model_training.py
+++ model_training.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import torch
+from sklearn.model_selection import train_test_split
 
 
 class Model:
@@ -16,6 +17,10 @@ class Model:
         raise NotImplementedError
 
 
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+
 class PyTorchModel(Model):
     def __init__(self, model, criterion, optimizer, device):
         self.model = model
@@ -23,37 +28,77 @@ class PyTorchModel(Model):
         self.optimizer = optimizer
         self.device = device
 
-    def fit(self, dataloader, num_epochs=25):
+    def _create_dataloader(self, X, y, batch_size=32, shuffle=True):
+        X_tensor = torch.tensor(X, dtype=torch.float32)
+        y_tensor = torch.tensor(y, dtype=torch.long)
+        dataset = TensorDataset(X_tensor, y_tensor)
+        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+    def fit(self, X_train, y_train, num_epochs=25, validation_split=0.1, patience=3):
         self.model.to(self.device)
+
+        # Split training data for validation
+        X_train_split, X_val_split, y_train_split, y_val_split = train_test_split(
+            X_train, y_train, test_size=validation_split
+        )
+
+        train_loader = self._create_dataloader(X_train_split, y_train_split)
+        val_loader = self._create_dataloader(X_val_split, y_val_split, shuffle=False)
+
+        best_val_loss = float('inf')
+        epochs_no_improve = 0
+
         for epoch in range(num_epochs):
+            self.model.train()
             running_loss = 0.0
-            for i, (inputs, labels) in enumerate(dataloader, 0):
+
+            # Training loop
+            for inputs, labels in train_loader:
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
-
                 self.optimizer.zero_grad()
-
                 outputs = self.model(inputs)
                 loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
-
                 running_loss += loss.item()
-                if i % 100 == 99:  # Print every 100 mini-batches
-                    print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 100:.3f}')
-                    running_loss = 0.0
 
-    def predict(self, dataloader):
+            # Validation loop
+            val_loss = 0.0
+            self.model.eval()
+            with torch.no_grad():
+                for inputs, labels in val_loader:
+                    inputs, labels = inputs.to(self.device), labels.to(self.device)
+                    outputs = self.model(inputs)
+                    loss = self.criterion(outputs, labels)
+                    val_loss += loss.item()
+
+            avg_train_loss = running_loss / len(train_loader)
+            avg_val_loss = val_loss / len(val_loader)
+
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}')
+
+            # Check for early stopping
+            if avg_val_loss < best_val_loss:
+                best_val_loss = avg_val_loss
+                epochs_no_improve = 0
+            else:
+                epochs_no_improve += 1
+                if epochs_no_improve == patience:
+                    print('Early stopping triggered')
+                    break
+
+    def predict(self, X_test):
         self.model.to(self.device)
         self.model.eval()
+        test_loader = self._create_dataloader(X_test, np.zeros(len(X_test)), batch_size=32, shuffle=False)
         predictions = []
         with torch.no_grad():
-            for inputs, _ in dataloader:
+            for inputs, _ in test_loader:
                 inputs = inputs.to(self.device)
                 outputs = self.model(inputs)
                 _, predicted = torch.max(outputs.data, 1)
                 predictions.append(predicted.cpu().numpy())
         return np.concatenate(predictions)
-
 
 
 def evaluate_classification_model(model, X_train, y_train, X_test, y_test):
--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,5 @@
 from data_loading import load_dataset, create_input_space, split_data_by_candidate, augment_data
-from data_processing import preprocess_signals, normalize_data
+from data_processing import preprocess_signals, normalize_data, trim_signals
 
 
 def loading_pipeline(test_size=5):
@@ -17,5 +17,6 @@ def loading_pipeline(test_size=5):
     X_test = normalize_data(X_test)
 
     augmented_X, augmented_y = augment_data(X_train, y_train)
+    X_test = trim_signals(X_test)
 
     return augmented_X, augmented_y, X_test, y_test
